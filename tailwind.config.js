a9313fc651c48858cf5168777e25283f788fb771
mateorey22 <149890580+mateorey22@users.noreply.github.com>
2025-01-11 12:35:52 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  purge: [
+    './*.html',
+    './js/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
